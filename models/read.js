a26f9cbb9fded677b5efa35b982ea1f889054ff8
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2023-08-12 10:04:56 +0530

    try catch for axios api calls

--- models/read.js
+++ models/read.js
@@ -1,15 +1,21 @@
 const env = require('dotenv').config();
 const axios = require('axios');
 
-let readDb = async function (){
-    let readData = await axios.get(process.env.JSONBIN_URL+"/latest", { 
-            headers: {
-              "Content-Type":"application/json",
-              "X-Master-Key":process.env.MASTER_KEY
-            }
-        });
+let readDb = async function () {
+  try {
+    let readData = await axios.get(process.env.JSONBIN_URL + "/latest", {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.MASTER_KEY
+      }
+    });
 
     return readData.data.record;
+
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 module.exports = { readDb }
