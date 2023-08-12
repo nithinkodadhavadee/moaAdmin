30ad1f99a80afd6580eca1e401f3d51ea3960564
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2023-08-12 09:54:56 +0530

    commit gitignore file

--- models/update.js
+++ models/update.js
@@ -1,13 +1,18 @@
 const env = require('dotenv').config();
 const axios = require('axios');
 
-let updateDb = async function (updateData){
-    await axios.put(process.env.JSONBIN_URL,JSON.stringify(updateData), {
-    headers: {
-      "Content-Type":"application/json",
-      "X-Master-Key":process.env.MASTER_KEY
-    }
-})
+let updateDb = async function (updateData) {
+  try {
+    await axios.put(process.env.JSONBIN_URL, JSON.stringify(updateData), {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.MASTER_KEY
+      }
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = { updateDb }
